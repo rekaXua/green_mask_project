5dc1a532fee266498c11cc27600e1e55030a55bc
rekaXua <34725775+rekaXua@users.noreply.github.com>
2019-08-20 01:24:43 +0200

    error handling

--- green_mask_project/green_mask_project.py
+++ green_mask_project/green_mask_project.py
@@ -9,6 +9,7 @@ from PIL import Image
 
 rootdir = "../decensor_input_original"
 files = glob.glob(rootdir + '/**/*.png', recursive=True)
+err_files=[]
 
 #Options
 GBlur=3
@@ -35,38 +36,44 @@ for masksize in range(LowRange, HighRange+1):
     img.save("patterns/pattern"+str(masksize)+"x"+str(masksize)+".png")
 
 for f in files:
-	print(f)
+	try:
+		print(f)
 	
-	img_C = Image.open(f).convert("RGBA")
-	x, y = img_C.size
-	card = Image.new("RGBA", (x, y), (255, 255, 255, 3))
-	card.paste(img_C, (0, 0, x, y), img_C)
-	card.save(os.path.dirname(f) + '/temp.png', format="png")
-	os.replace(os.path.dirname(f) + '/temp.png', f)
+		img_C = Image.open(f).convert("RGBA")
+		x, y = img_C.size
+		card = Image.new("RGBA", (x, y), (255, 255, 255, 3))
+		card.paste(img_C, (0, 0, x, y), img_C)
+		card.save(os.path.dirname(f) + '/temp.png', format="png")
+		os.replace(os.path.dirname(f) + '/temp.png', f)
 	
-	img_rgb = cv2.imread(f)
+		img_rgb = cv2.imread(f)
 	
-	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-	img_gray = cv2.Canny(img_gray,CannyTr1,CannyTr2)
-	img_gray = 255-img_gray
-	img_gray = cv2.GaussianBlur(img_gray,(GBlur,GBlur),0)
+		img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+		img_gray = cv2.Canny(img_gray,CannyTr1,CannyTr2)
+		img_gray = 255-img_gray
+		img_gray = cv2.GaussianBlur(img_gray,(GBlur,GBlur),0)
 
-#	cv2.imwrite('output_gray.png', img_gray)     #DEBUG
+#		cv2.imwrite('output_gray.png', img_gray)     #DEBUG
 
-	for i in range(LowRange,HighRange+1):
-		pattern_filename = "patterns/pattern"+str(i)+"x"+str(i)+".png"
-		template = cv2.imread(pattern_filename, 0)
-		w, h = template.shape[::-1]
+		for i in range(LowRange,HighRange+1):
+			pattern_filename = "patterns/pattern"+str(i)+"x"+str(i)+".png"
+			template = cv2.imread(pattern_filename, 0)
+			w, h = template.shape[::-1]
 
-		img_detection = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-		threshold = DetectionTr
-		loc = np.where(img_detection >= threshold)
-		for pt in zip(*loc[::-1]):
-#			cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)     #DEBUG
-			cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), -1)
-#		cv2.imwrite('output_progress_'+str(i)+'.png', img_rgb)     #DEBUG
+			img_detection = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+			threshold = DetectionTr
+			loc = np.where(img_detection >= threshold)
+			for pt in zip(*loc[::-1]):
+#				cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)     #DEBUG
+				cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), -1)
+#			cv2.imwrite('output_progress_'+str(i)+'.png', img_rgb)     #DEBUG
 
-	f=f.replace("/decensor_input_original", "/decensor_input", 1)
-	os.makedirs(os.path.dirname(f), exist_ok=True)
-	cv2.imwrite(os.path.dirname(f) + '/temp.png', img_rgb)
-	os.replace(os.path.dirname(f) + '/temp.png', f)
+		f=f.replace("/decensor_input_original", "/decensor_input", 1)
+		os.makedirs(os.path.dirname(f), exist_ok=True)
+		cv2.imwrite(os.path.dirname(f) + '/temp.png', img_rgb)     #hack for non-unicode chars
+		os.replace(os.path.dirname(f) + '/temp.png', f)
+	except:
+		err_files.append(os.path.basename(f))
+		pass
+print("Could not mask those files: ") 
+print(err_files)
