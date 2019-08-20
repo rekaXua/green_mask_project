800e82f4ff51f7c206fcfb46b2c55acab2998c24
rekaXua <34725775+rekaXua@users.noreply.github.com>
2019-08-21 00:10:05 +0200

    add_previews

--- green_mask_project/green_mask_project.py
+++ green_mask_project/green_mask_project.py
@@ -19,14 +19,15 @@ LowRange = 5
 HighRange = 20
 DetectionTr = 0.3
 
-change_set = raw_input("Would you like to change default settings? [y/N]") or "n"
+Prews = int(input("How many previews would you like to see (every closed preview will already save detected file): [0] ") or "0")
+change_set = input("Would you like to change default settings? [y/N] ") or "n"
 if change_set == "y":
-	GBlur = raw_input("Enter the Gaussian Blur value: [3]") or "3"
-	CannyTr1 = raw_input("Enter the Canny Edge Detector Treshold 1 value: [1]") or "1"
-	CannyTr2 = raw_input("Enter the Canny Edge Detector Treshold 2 value: [35]") or "35"
-	LowRange = raw_input("Enter the Minimal Resolution Of Patterns: [5]") or "5"
-	HighRange = raw_input("Enter the Maximal Resolution Of Patterns: [20]") or "20"
-	DetectionTr = raw_input("Enter the Detection Treshold value: [0.3]") or "0.3"
+	GBlur = input("Enter the Gaussian Blur value: [3] ") or "3"
+	CannyTr1 = input("Enter the Canny Edge Detector Treshold 1 value: [1] ") or "1"
+	CannyTr2 = input("Enter the Canny Edge Detector Treshold 2 value: [35] ") or "35"
+	LowRange = input("Enter the Minimal Resolution Of Patterns: [5] ") or "5"
+	HighRange = input("Enter the Maximal Resolution Of Patterns: [20] ") or "20"
+	DetectionTr = input("Enter the Detection Treshold value: [0.3] ") or "0.3"
 
 #Create patterns
 for masksize in range(LowRange, HighRange+1):
@@ -49,15 +50,14 @@ for masksize in range(LowRange, HighRange+1):
 for f in files:
 	try:
 		print(f)
-		#Hack for non-typical images
+		#Dirty hack for non-typical images and non-unicode names (I blame CV2 for this)
 		img_C = Image.open(f).convert("RGBA")
 		x, y = img_C.size
 		card = Image.new("RGBA", (x, y), (255, 255, 255, 3))
 		card.paste(img_C, (0, 0, x, y), img_C)
-		card.save(os.path.dirname(f) + '/temp.png', format="png")
-		os.replace(os.path.dirname(f) + '/temp.png', f)
+		card.save('temp.png', format="png")
 	
-		img_rgb = cv2.imread(f)
+		img_rgb = cv2.imread('temp.png')
 		img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 		img_gray = cv2.Canny(img_gray,CannyTr1,CannyTr2)
 		img_gray = 255-img_gray
@@ -77,17 +77,19 @@ for f in files:
 #				cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)     #DEBUG
 				cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,0), -1)
 #			cv2.imwrite('output_progress_'+str(i)+'.png', img_rgb)     #DEBUG
-		#Change save path
+		#Change path to save folder
 		f=f.replace("/decensor_input_original", "/decensor_input", 1)
-		#Preview
-		cv2.imshow('window1', img_rgb)
-#		cv2.imshow('window2', img_gray)     #DEBUG
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+		#Previews
+		if Prews > 0:
+			cv2.imshow('Preview! Press Enter or close to save', img_rgb)
+#			cv2.imshow('preview_gray', img_gray)     #DEBUG
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+			Prews -= 1
 		#Save file
 		os.makedirs(os.path.dirname(f), exist_ok=True)
-		cv2.imwrite(os.path.dirname(f) + '/temp.png', img_rgb)     #hack for non-unicode chars
-		os.replace(os.path.dirname(f) + '/temp.png', f)
+		cv2.imwrite('temp_out.png', img_rgb)     #still a hack for non-unicode names
+		os.replace('temp_out.png', f)
 	except Exception as Exception:
 		err_files.append(os.path.basename(f) + ": " + str(Exception))
 		pass
