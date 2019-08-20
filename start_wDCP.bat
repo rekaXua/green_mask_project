b78c1e44e05f6b9ba6667dfd73b983e043790f24
rekaXua <34725775+rekaXua@users.noreply.github.com>
2019-08-21 00:36:29 +0200

    start_skripts

---
+++ start_wDCP.bat
@@ -0,0 +1,4 @@
+cd green_mask_project
+python green_mask_project.py
+cd ..
+pause
---
+++ start_wDCP.sh
@@ -0,0 +1,3 @@
+cd green_mask_project
+python3 green_mask_project.py
+read -n1 -r -p "Press any key to continue..." key
